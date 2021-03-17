4e77900bb225fa3ef70f6c15a815dc80408da4bc
JochenFM <jochen_mayer@gmx.de>
2021-03-17 11:15:10 +0000

    need to restart gitpod as python3 does not start up due to 'No module named flask_pymongo'

--- app.py
+++ app.py
@@ -30,6 +30,4 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
-
 # needs to be updated to debug = False prior to submission
