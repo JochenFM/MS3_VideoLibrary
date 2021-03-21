a13cc59f2a1fd1f2de98247403867e41cad3c0a0
JochenFM <jochen_mayer@gmx.de>
2021-03-21 13:05:06 +0000

    changed slider to carousel and that works now with basic features

--- static/js/script.js
+++ static/js/script.js
@@ -3,10 +3,22 @@ const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {edge: "right"});
 
 //slider
-const slider = document.querySelector('.slider');
-M.Sidenav.init(slider, {
-    indicators: false,
+
+const carousel = document.querySelector('.carousel');
+M.Carousel.init(carousel,{
     height: 500,
     transition: 500,
-    interval: 6000    
-});
+    interval: 6000,
+    fullWidth: true,
+    indicators: true
+  });
+
+
+//slider
+//const slider = document.querySelector('.slider');
+//M.Sidenav.init(slider, {
+  //  indicators: false,
+    //height: 500,
+    //transition: 500,
+    //interval: 6000    
+//});
