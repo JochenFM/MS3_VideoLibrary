529619ab8fa690f8bd9fd73d7979eaa36ea91a7d
JochenFM <jochen_mayer@gmx.de>
2021-03-25 11:03:12 +0000

    custom JS added to validate Materialize category selector

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,26 @@
 //sidenav
 const sideNav = document.querySelector('.sidenav');
-M.Sidenav.init(sideNav, {edge: "right"});
+M.Sidenav.init(sideNav, { edge: "right" });
 
 //slider
 
 const carousel = document.querySelector('.carousel');
-M.Carousel.init(carousel,{
+M.Carousel.init(carousel, {
     indicators: true,
     height: 900,
     transition: 500,
     interval: 6000
-  });
+});
 
 
-  //category selector
-   
+//category selector
+
 const select = document.querySelector('select');
 M.FormSelect.init(select, {
-  });
+});
 
 
-  //datepicker
+//datepicker
 
 const datepicker = document.querySelector('.datepicker');
 M.Datepicker.init(datepicker, {
@@ -29,13 +29,13 @@ M.Datepicker.init(datepicker, {
     showClearBtn: true,
     i18n: {
         done: "Select"
-        }
-    });
+    }
+});
 
 
 const collapsible = document.querySelector('.collapsible');
 M.Collapsible.init(collapsible, {
-  });
+});
 
 
 
@@ -43,8 +43,48 @@ M.Collapsible.init(collapsible, {
 //slider
 //const slider = document.querySelector('.slider');
 //M.Sidenav.init(slider, {
-  //  indicators: false,
-    //height: 500,
-    //transition: 500,
-    //interval: 6000    
+//  indicators: false,
+//height: 500,
+//transition: 500,
+//interval: 6000    
 //});
+
+
+//category select validation from CI's TIm Nelson
+
+validateMaterializeSelect();
+function validateMaterializeSelect() {
+    let classValid = "border-bottom: 1px solid #4caf50; box-shadow: 0 1px 0 0 #4caf50;";
+    let classInvalid = "border-bottom: 1px solid #f44336; box-shadow: 0 1px 0 0 #f44336;";
+    let selectValidate = document.querySelector("select.validate");
+    let selectWrapperInput = document.querySelector(".select-wrapper input.select-dropdown");
+    if (selectValidate.hasAttribute("required")) {
+        selectValidate.style.cssText = "display: block; height: 0; padding: 0; width: 0; position: absolute;";
+    }
+    selectWrapperInput.addEventListener("focusin", (e) => {
+        e.target.parentNode.addEventListener("change", () => {
+            ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
+            for (let i = 0; i < ulSelectOptions.length; i++) {
+                if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled") {
+                    e.target.style.cssText = classValid;
+                }
+            }
+        });
+    });
+    selectWrapperInput.addEventListener("click", (e) => {
+        ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
+        for (let i = 0; i < ulSelectOptions.length; i++) {
+            if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled" && ulSelectOptions[i].style.backgroundColor == "rgba(0, 0, 0, 0.03)") {
+                e.target.style.cssText = classValid;
+            } else {
+                e.target.addEventListener("focusout", () => {
+                    if (e.target.parentNode.childNodes[3].hasAttribute("required")) {
+                        if (e.target.style.borderBottom != "1px solid rgb(76, 175, 80)") {
+                            e.target.style.cssText = classInvalid;
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
